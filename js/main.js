a8ec6e9424742a9b3c15b342664aee8fd83beaf2
htq121003 <htq121003@gmail.com>
2024-06-18 11:44:40 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -77,3 +77,27 @@
     
 })(jQuery);
 
+
+    //Quantity of products
+    $('.quantity button').on('click', function () {
+        var button = $(this);
+        var oldValue = button.parent().parent().find('input').val();
+        if (button.hasClass('btn-plus')) {
+            var newVal = parseFloat(oldValue) + 1;
+        } else {
+            if (oldValue > 0) {
+                var newVal = parseFloat(oldValue) - 1;
+            } else {
+                newVal = 0;
+            }
+        }
+        button.parent().parent().find('input').val(newVal);
+    });
+
+
+//Remove cart
+$(document).ready(function () {
+    $('.xoa').click(function () {
+        $(this).closest('tr').hide();
+    });
+});  
